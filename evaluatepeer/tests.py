bf6740d2e29307e7747241c2efd0e1d2c592650d
Aaron <abeatson411@gmail.com>
2019-05-10 06:14:28 +1000

    Basic index view returns minimal HTML informed by unittest

--- evaluatepeer/tests.py
+++ evaluatepeer/tests.py
@@ -1,5 +1,7 @@
 from django.urls import resolve
 from django.test import TestCase
+from django.http import HttpRequest
+
 from evaluatepeer.views import evaluate
 
 
@@ -9,3 +11,11 @@ class EvaluatePageTest(TestCase):
     def test_root_url_resolves_to_evaluate_view(self):
         found = resolve('/')
         self.assertEqual(found.func, evaluate)
+
+    def test_index_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = evaluate(request)
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>PAF</title>', html)
+        self.assertTrue(html.endswith('</html>'))
--- evaluatepeer/views.py
+++ evaluatepeer/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
 # Create your views here.
-def evaluate():
-    pass
+def evaluate(request):
+    return HttpResponse("<html><title>PAF</title></html>")
